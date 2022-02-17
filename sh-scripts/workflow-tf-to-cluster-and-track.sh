11493db80d0d6bd4395ca9fbd4a8bd32d2264c6b
Andry Rakotozafindrabe <arakotoz@cern.ch>
2022-02-17 12:10:12 +0100

    improve directions of use

--- sh-scripts/workflow-tf-to-cluster-and-track.sh
+++ sh-scripts/workflow-tf-to-cluster-and-track.sh
@@ -1,40 +1,29 @@
 #!/usr/bin/env bash
 
-# make sure that you are using O2 from CVMFS
-# via for e.g. 
-# > /cvmfs/alice.cern.ch/bin/alienv enter VO_ALICE@O2PDPSuite::nightly-20211026-1
-# or 
-# > /cvmfs/alice.cern.ch/bin/alienv enter VO_ALICE@O2Physics::nightly-20211026-1
+# -------------------------------------------
+# Make sure that you are using O2 from CVMFS
+# - on lxplus: 
+#   > /cvmfs/alice.cern.ch/bin/alienv enter VO_ALICE@O2Physics::nightly-20220211-1
+# - locally:
+#   > alienv enter O2Physics/latest
+# -------------------------------------------
+# Make sure you have initialised your alien
+#   > alien.py
+# -------------------------------------------
+# make the workflow available
+#   > source workflow-tf-to-cluster-and-track.sh
+# -------------------------------------------
 
-function groupfiles () {
-    # create a subgroup of files for reco
-    # from a list of files, obtained for e.g. with:
-    #      alien_ls -c /alice/data/2021/OCT/504980/raw/*.tf > tflist
-    # or
-    #      alien_ls -c /alice/data/2021/OCT/504980/raw/*.root > ctflist
-    # use:
-    #      FilesPerGroup=15 groupfiles ctflist
-    # alternatively, use the bash program split instead:
-    #      split -l 15 -d ctflist group_
-    # the command used here was:
-    #      split -l 30 -a 3 -d raw_0100_tf a_raw_0100_tf_
-    FilesPerGroup=${FilesPerGroup:-"50"}
-    GroupCounter=0
-    FileCounter=0
-    rm group_*
-    for file in $(cat $@)
-    do
-	echo ${file} >> group_${GroupCounter}
-	((FileCounter++))
 
-	if [ ${FileCounter} -eq ${FilesPerGroup} ]
-	then
-            ((GroupCounter++))
-            FileCounter=0
-	fi          
-    done
-}
-export -f groupfiles
+# From a large list of files, obtained for e.g. with:
+#   > mkdir -p list-tf; alien_ls -c /alice/data/2021/NOV/505713/raw/0100/*.tf > list-tf/raw_0100_tf
+# create smaller subgroup of files to be used in the workflow:
+#   > cd list-tf; split -l 20 -a 3 -d raw_0110_tf a_raw_0110_tf_
+# create jobs to call the workflow for each subgroup:
+#   > mkdir -p ../list-jobs; ls -1 a_raw_0110_tf_* | awk '{ print "go-tf-to-cluster-track-b0 "$1}' > ../list-jobs
+#   > cd .. ; mv list-tf/a_raw_0110_tf_* .
+# run the jobs (open one lxplus node terminal to execute each job_##):
+#   > ./job_00
 
 
 function go-tf-to-cluster-track() {
