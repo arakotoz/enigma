e6138f80c2cace90a0b9cbb400db32ca031652c3
Andry Rakotozafindrabe <arakotoz@cern.ch>
2022-11-30 01:06:18 +0100

    modify grid job submission to use pass 2

--- grid/reco.sh
+++ grid/reco.sh
@@ -14,7 +14,7 @@ echo ""
 
 localCcdbBaseDir="${currentDir}/ccdb"
 
-geomFilenameAligned="pass1_o2sim_geometry-aligned.root"
+geomFilenameAligned="pass2_o2sim_geometry-aligned.root"
 if [ ! -e "${geomFilenameAligned}" ]; then
     echo "Cannot find $geomFilenameAligned, exit"
     exit 1
